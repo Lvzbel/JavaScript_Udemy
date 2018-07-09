49622f0aaae5283113d31d64f5236757050f37c7
Rodrigo <26789151+Lvzbel@users.noreply.github.com>
2018-07-08 23:59:14 -0700

    Promises completed

--- hangman/app.js
+++ hangman/app.js
@@ -15,12 +15,43 @@ window.addEventListener('keypress', function (e) {
 
 getPuzzle('2').then((puzzle) => {
   console.log(puzzle)
-}, (err) => {
-  console.log(`Error: ${err}`)
+}).catch((err) => {
+  console.log(err)
 })
 
 getCountry('SV').then((country) => {
-  console.log(`Country Name: ${country.name}`)
-}, (err) => {
-  console.log(`Error: ${err}`)
+  console.log(country.name);
+}).catch((err) => {
+  console.log(err);
 })
+
+getLocation().then((data) => {
+  console.log(`City: ${data.city}, Region: ${data.region}, Country: ${data.country}`);
+}).catch((err) => {
+  console.log(err)
+})
+
+getLocation().then((data) => {
+  return getCountry(data.country)
+}).then((country) => {
+    console.log(`Based on your IP you are currently located in ${country.name}`)
+}).catch((err) => {
+  console.log(err)
+})
+// getCountry('SV').then((country) => {
+//   console.log(`Country Name: ${country.name}`)
+// }, (err) => {
+//   console.log(`Error: ${err}`)
+// })
+
+// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+//   if (response.status === 200) {
+//     return response.json()
+//   } else {
+//     throw new Error('Unable to fetch the puzzle')
+//   }
+// }).then((data) => {
+//   console.log(data.puzzle)
+// }).catch((err) => {
+//   console.log(err)
+// })
--- hangman/request.js
+++ hangman/request.js
@@ -1,33 +1,53 @@
-const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
-  const request = new XMLHttpRequest();
+const getPuzzle = (wordCount) => {
+  return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+    if (response.status === 200) {
+      return response.json()
+    } else {
+      throw new Error('Unable to fetch puzzle')
+    }
+  }).then((data) => {
+    return data.puzzle
+  })
+}
 
-  request.addEventListener('readystatechange', (e) => {
+// const getCountry = (countryCode) => new Promise((resolve, reject) => {
+//   const countryRequest = new XMLHttpRequest();
 
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      const data = JSON.parse(e.target.responseText)
-      resolve(data.puzzle)
-    } else if (e.target.readyState === 4) {
-      reject('An error has taken place')
+//   countryRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//       const data = JSON.parse(e.target.responseText);
+//       const country = data.find((country) => country.alpha2Code === countryCode);
+//       resolve(country)
+//     } else if (e.target.readyState === 4) {
+//       reject('Unable to fetch country')
+//     }
+//   })
+
+//   countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+//   countryRequest.send();
+// })
+
+// THE ONE UNDER IS THE SAME THING BUT USIN THE FETCH METHOD RATHER THAN XMLHttpRequest() and then creating a promise from it
+// The fetch method already returns a promise
+
+const getCountry = (countryCode) => {
+  return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
+    if (response.status === 200) {
+      return response.json()
+    } else {
+      throw new Error('Unable to fetch country data')
+    }
+  }).then((countries) => {
+    return countries.find((country) => country.alpha2Code === countryCode)
+  })
+}
+
+const getLocation = () => {
+  return fetch('https://ipinfo.io/json?token=b167e4d3c3329b').then((response) => {
+    if (response.status === 200) {
+      return response.json()
+    } else {
+      throw new Error('Unable to fetch IP data')
     }
   })
-
-  request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-  request.send()
-})
-
-const getCountry = (countryCode) => new Promise((resolve, reject) => {
-  const countryRequest = new XMLHttpRequest();
-
-  countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      const data = JSON.parse(e.target.responseText);
-      const country = data.find((country) => country.alpha2Code === countryCode);
-      resolve(country)
-    } else if (e.target.readyState === 4) {
-      reject('Unable to fetch country')
-    }
-  })
-
-  countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-  countryRequest.send();
-})
+}
