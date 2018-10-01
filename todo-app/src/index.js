e6c5aa81a410a5548ea424ae46ae4c48468f7d2f
Rodrigo <rcoto@protonmail.com>
2018-10-01 10:22:05 -0700

    Filter fuctions are working properly

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,3 +1,14 @@
+import { getFilters, setFilters } from './filters'
+setFilters({
+    searchText: 'Hevov',
+    hideCompleted: false
+})
+console.log(getFilters());
+setFilters({
+    searchText: 'Hevo',
+    hideCompleted: true
+})
+console.log(getFilters());
 // Set up index.html to load the bundle
 // Make sure to load uuid via an npm module when necessary
 
